d6b69c6fe7cc860a76af90e0da3a82cfec033721
lizhijun20@126.com <lizhijun20@126.com>
2013-10-01 13:39:46 +0800

    数据库配置文件修改

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '717df5eb';
+$db['default']['password'] = '';
 $db['default']['database'] = 'iyourl'; //区分大小写
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
